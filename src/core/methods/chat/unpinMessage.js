485a8f21e8e1da9f48518053bd5f21cee618bc12
ФОЗАН <nazar.korkin@mail.ru>
2025-03-28 13:05:26 +0300

    update

--- src/core/methods/chat/unpinMessage.js
+++ src/core/methods/chat/unpinMessage.js
@@ -4,17 +4,16 @@
  * @returns {Promise<object>} - Результат удаления закрепленного сообщения.
  */
 async function unpinMessage(chatId) {
-    if (!chatId || typeof chatId !== 'number') {
-      throw new Error('Некорректный chatId: требуется число.');
+    if (!chatId || typeof chatId !== "number") {
+        throw new Error("Некорректный chatId: требуется число.");
     }
-  
+
     try {
-      const response = await this.apiClient.request('DELETE', `chats/${chatId}/pin`);
-      return response;
+        const response = await this.apiClient.request("DELETE", `chats/${chatId}/pin`, { quiry: { chat_id: chatId } });
+        return response;
     } catch (error) {
-      throw new Error(`Ошибка при удалении закрепленного сообщения: ${error.message}`);
+        throw new Error(`Ошибка при удалении закрепленного сообщения: ${error.message}`);
     }
-  }
-  
-  module.exports = unpinMessage;
-  
+}
+
+module.exports = unpinMessage;
